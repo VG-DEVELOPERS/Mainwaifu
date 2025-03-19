4b67b0425b29d49c5cf6230cce2b94131c8bc133
VG-DEVELOPERS <152973395+VG-DEVELOPERS@users.noreply.github.com>
2025-03-19 09:26:28 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -35,7 +35,7 @@ Grabberu = Client("Grabber", api_id, api_hash, bot_token=TOKEN)
 client = AsyncIOMotorClient(mongo_url)
 db = client['Character_catchers']
 collection = db['anime_characterss']
-user_totals_collection = db['user_totalss']
-user_collection = db["user_collections"]
+user_totals_collection = db['user_totalsss']
+user_collection = db["user_collectionss"]
 group_user_totals_collection = db['group_user_totals']
 top_global_groups_collection = db['top_global_groupss']
